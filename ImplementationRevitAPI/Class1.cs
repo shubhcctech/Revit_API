4560571f11a762ab325293efa84a30b2af3c2177
shubhcctech <shubham.kurkute@cctech.co.in>
2024-04-10 19:31:03 +0530

    Dev push

---
+++ ImplementationRevitAPI/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationRevitAPI
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+
+    public class Class1 : IExternalCommand
+    {
+        Result IExternalCommand.Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Result.Succeeded;
+        }
+    }
+}
